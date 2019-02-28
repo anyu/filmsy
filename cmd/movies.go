e979256ba974705410f21bca6af22a9c1f97ce69
An Yu <ayu3500@gmail.com>
2019-02-27 22:29:53 -0500

    (feat) Retrieve movies by year

--- cmd/movies.go
+++ cmd/movies.go
@@ -2,9 +2,16 @@ package cmd
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/spf13/cobra"
 )
+
+const moviesByYearAPIURL = "https://api.themoviedb.org/3/discover/movie?api_key=%s&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&year=%s"
 
 var moviesCmd = &cobra.Command{
 	Use:   "movies",
@@ -16,6 +23,24 @@ func init() {
 	rootCmd.AddCommand(moviesCmd)
 }
 
-func movies(c *cobra.Command, _ []string) {
-	fmt.Println("OY")
+func movies(c *cobra.Command, args []string) {
+	getMovies(args[0])
+}
+
+func getMovies(year string) error {
+	err := godotenv.Load()
+	if err != nil {
+		log.Print("error loading from env file")
+	}
+	apiKey := os.Getenv("API_KEY")
+	requestURL := fmt.Sprintf(moviesByYearAPIURL, apiKey, year)
+
+	response, err := http.Get(requestURL)
+	if err != nil {
+		panic("TODO")
+	}
+	respBody, _ := ioutil.ReadAll(response.Body)
+
+	fmt.Printf("%s", respBody)
+	return err
 }
--- go.mod
+++ go.mod
@@ -1,6 +1,7 @@
 module github.com/anyu/filmsy
 
 require (
+	github.com/joho/godotenv v1.3.0
 	github.com/spf13/cobra v0.0.3
 	github.com/spf13/pflag v1.0.3 // indirect
 )
